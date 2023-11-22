9e2cfffa710914b8db839b03b7892116d36854fc
77laxmi <lokhandelax2197@gmail.com>
2023-11-22 21:21:02 +0530

    Shop file is created

---
+++ Shop.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../Component/Hero/Hero'
+import Popular from '../Component/Popular/Popular'
+import Offer from '../Component/Offers/Offer'
+import NewCollections from '../Component/NewCollections/NewCollections'
+import NewsLetter from '../Component/NewsLetter/NewsLetter'
+
+const Shop = () => {
+  return (
+    <>
+        <Hero/>
+        <Popular/>
+          <Offer/>
+          <NewCollections/>
+          <NewsLetter/>
+  
+    </>
+        
+  )
+}
+
+export default Shop
